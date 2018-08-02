c81f0fb8ed8f251f29f0548cdaeee6ad8d12b6fc
Jon Fairbanks <fairplay89@gmail.com>
2018-08-01 21:40:07 -0700

    Initial API Support

--- server.js
+++ server.js
@@ -8,6 +8,7 @@
 //    - LOGGING: If 'true', an access.log will be created for incoming site requests using Morgan logging
 //    - RATE_LIMIT: If 'true', enables DDoS and RateLimit protections through Express
 //    - SITE_ROOT: If set to a string, that path will be used as the default site root instead of the default of 'public'
+//    - MODE: If set as `api`, ExpressHTTP will start in API mode and return a JSON response at /api.
 //
 const express = require('express'),
   Ddos = require('ddos'),
@@ -17,8 +18,11 @@ const express = require('express'),
   morgan = require('morgan'),
   session = require('express-session'),
   lusca = require('lusca')
+  mongoose = require('mongoose');
+  bodyParser = require('body-parser');
 
 const app = express()
+const router = express.Router();
 
 // Setup Session Management & Lusca
 if(!process.env.SESSION_SECRET){ console.warn("SESSION_SECRET not passed. Using a default value."); }
@@ -29,17 +33,12 @@ app.use(session({
   cookie: { secure: true }
 }))
 
-app.use(lusca({
-  csrf: true,
-  csp: false, // Set a valid CSP if desired - https://hacks.mozilla.org/2016/02/implementing-content-security-policy/
-  xframe: 'SAMEORIGIN',
-  hsts: {maxAge: 31536000, includeSubDomains: true, preload: true},
-  xssProtection: true,
-  nosniff: true,
-  referrerPolicy: 'same-origin'
-}))
+if(process.env.MODE="web"){
+
+}
 
 // Set additional headers and other middlewares if required
+app.disable('x-powered-by') // Disables Express' "X-Powered-By" Header
 app.use(function(req, res, next) {
   res.setHeader('X-Timestamp', Date.now()) // Tag all requests with a timestamp
   res.setHeader('X-Words-of-Wisdom', '"You come at the king, you best not miss." - Omar Little') // Yo dawg...
@@ -58,7 +57,7 @@ if(process.env.RATE_LIMIT == true) {
   const limiter = new RateLimit({
     windowMs: 15*60*1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
-    delayMs: 0 // disable delaying - full speed until the max limit is reached
+    delayMs: 0 // Disable delaying - full speed until the max limit is reached
   })
   app.use(ddos.express)
   app.use('/', limiter)
@@ -67,7 +66,35 @@ if(process.env.RATE_LIMIT == true) {
 var port = null;
 if(process.env.PORT){ port = process.env.PORT; }else{ port = 8888; } // Default port is 8888 unless passed
 
-app.disable('x-powered-by') // Disables Express' "X-Powered-By" Header
-app.use('/', express.static(path.join(__dirname, process.env.SITE_ROOT || 'public'))) // Use the ENV defined site root or default "public"
-
-app.listen(port, () => console.log('App Listening on Port ' + port))
+if(process.env.MODE='api'){
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  //To prevent errors from Cross Origin Resource Sharing, set headers to allow CORS with middleware
+  app.use(function(req, res, next) {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Credentials', 'true');
+   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+   res.setHeader('Cache-Control', 'no-cache'); // Remove caching so we get the most recent values
+   next();
+  });
+  //now we can set the route path & initialize the API
+  router.get('/', function(req, res) {
+   res.json({ message: 'API Initialized!'})
+  });
+  app.use('/api', router)
+  //starts the server and listens for requests
+  app.listen(port, () => console.log('API Listening on Port ' + port))
+}else {
+  app.use(lusca({
+    csrf: true,
+    csp: false, // Set a valid CSP if desired - https://hacks.mozilla.org/2016/02/implementing-content-security-policy/
+    xframe: 'SAMEORIGIN',
+    hsts: {maxAge: 31536000, includeSubDomains: true, preload: true},
+    xssProtection: true,
+    nosniff: true,
+    referrerPolicy: 'same-origin'
+  }))
+  app.use('/', express.static(path.join(__dirname, process.env.SITE_ROOT || 'public'))) // Use the ENV defined site root or default "public"
+  app.listen(port, () => console.log('App Listening on Port ' + port))
+}
